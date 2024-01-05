bd682d2c942e2d130b9584341638be73931847e0
Joan Toro <joantoro@me.com>
2024-01-04 21:16:51 -0300

    Mejora en custom input

--- App/src/app/shared/components/custom-input/custom-input.component.ts
+++ App/src/app/shared/components/custom-input/custom-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-custom-input',
@@ -6,9 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./custom-input.component.scss'],
 })
 export class CustomInputComponent  implements OnInit {
-
+  @Input() label!: String;
+  @Input() type!: String;
+  @Input() placeholder!: String;
+  @Input() icon!: String;
+  
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+  }
 
 }
